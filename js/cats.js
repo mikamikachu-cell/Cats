33ced674cbc67f78fe6b62b34762c9a72acc5d8b
mikamikachu-cell <81223524+mikamikachu-cell@users.noreply.github.com>
2022-05-05 12:19:39 +0200

    Listing of cat races

---
+++ js/cats.js
@@ -0,0 +1,323 @@
+console.log('cats.js')
+//abyssin
+function factAbyssin(){
+    alert('When a cat chases its prey, it keeps its head level. Dogs and humans bob their heads up and down.');
+}
+
+//american curl
+function factAmericanCurl(){
+    alert('A cat\'s normal temperature varies around 101 degrees Fahrenheit.');
+}
+//american shorthair
+function factAmericanShort(){
+alert('A group of cats is called a “clowder.');
+}
+//american wirehair
+function factAmericanWire(){
+    alert('A cat\'s smell is their strongest sense, and they rely on this leading sense to identify people and objects; a feline\'s sense of smell is 14x better than a human\'s.');
+}
+//anatoli
+function factAnatoli(){
+    alert('In multi-cat households, cats of the opposite sex usually get along better.');
+}
+//angora turc
+function factAngora(){
+    alert('A cat can sprint at about thirty-one miles per hour.');
+}
+//asian
+function factAsisan(){
+    alert('A cat has 230 bones in its body. A human has 206. A cat has no collarbone, so it can fit through any opening the size of its head.')
+}
+//brumeAustralian
+function factBrume(){
+    alert('Fossil records from two million years ago show evidence of jaguars.');
+}
+//balinais
+function factBalinais(){
+    alert('"A queen (female cat) can begin mating when she is between 5 and 9 months old.');
+}
+//bengal
+function factBengal(){
+    alert('A cat will tremble or shiver when it is extreme pain.');
+}
+//bleu russe
+function factBlueRussian(){
+    alert('A cat only has the ability to move their jaw up and down, not side to side like a human can.');
+}
+//bobtail americain
+function factBobtail(){
+    alert('Cats don’t have sweat glands over their bodies like humans do. Instead, they sweat only through their paws.');
+
+}
+//bobtail des kourils
+function factBobtailKourils()
+{
+    alert('Lions are the only cats that live in groups, called prides. Every female within the pride is usually related.');
+}
+// bobtail_japonais 
+function factBobtailJapon(){
+    alert('Cats do not think that they are little people. They think that we are big cats. This influences their behavior in many ways.');
+}
+//bombay
+function factBombay(){
+    alert('Not every cat gets \\high\\\" from catnip. Whether or not a cat responds to it depends upon a recessive gene: no gene\"');
+}
+//brazilian shorthair
+function factBrazilianShort() {
+    alert('Cats should not be fed tuna exclusively, as it lacks taurine, an essential nutrient required for good feline health.  Make sure you have the proper Pet supplies to keep your cat happy and healthy.');
+    
+}
+//british longhair
+function factBritishLong() {
+    alert('The average cat food meal is the equivalent to about five mice.');
+    
+}
+//british shorthair
+function factBritishShort() {
+    alert('Unlike humans, cats do not need to blink their eyes on a regular basis to keep their eyes lubricated.');
+}
+//burmese
+function factBurmese() {
+    alert('Cats are now Britain\'s favourite pet: there are 7.7 million cats as opposed to 6.6 million dogs.');
+    
+}
+//burmilla
+function factBurmilla(){
+    alert('People who are allergic to cats are actually allergic to cat saliva or to cat dander. If the resident cat is bathed regularly the allergic people tolerate it better.');
+}
+//california rex
+function factCaliforniaRex(){
+    alert('Cats lap liquid from the underside of their tongue, not from the top.');
+}
+//california Spangled
+function factCaliforniaSpangled(){
+    alert('In the 1750s, Europeans introduced cats into the Americas to control pests.');
+}
+//ceylan
+function factCeylan(){
+    alert('The cat appears to be the only domestic companion animal not mentioned in the Bible.');
+}
+//chartreux
+function factChartreux(){
+    alert('Some common houseplants poisonous to cats include: English Ivy, iris, mistletoe, philodendron, and yew.');
+}
+//chantilly
+function factChantilly(){
+    alert('The first cat show was in 1871 at the Crystal Palace in London.');
+}
+//chausie
+function factChausie(){
+    alert('When a cats rubs up against you, the cat is marking you with it\'s scent claiming ownership.');
+}
+//colorpoint shorthair
+function factColorPoint() {
+    alert('A cat only has the ability to move their jaw up and down, not side to side like a human can.');   
+}
+//cornish rex
+function factCornishRex(){
+    alert('Julius Ceasar, Henri II, Charles XI, and Napoleon were all afraid of cats.')
+}
+//cymric
+function factCymric(){
+    alert('Since cats are so good at hiding illness, even a single instance of a symptom should be taken very seriously.');
+}
+//devon rex 
+function factDevonRex(){
+    alert('Of all the species of cats, the domestic cat is the only species able to hold its tail vertically while walking. All species of wild cats hold their tail horizontally or tucked between their legs while walking.');
+}
+//donskoy
+function factDonskoy(){
+    alert('The average litter of kittens is between 2 - 6 kittens.');
+}
+//european shorthair
+function factEuropeanShort(){
+    alert('Cats respond better to women than to men, probably due to the fact that women\'s voices have a higher pitch.');
+}
+//exotic shorthair
+function factExoticShort(){
+    alert('Cats lose almost as much fluid in the saliva while grooming themselves as they do through urination.');
+}
+//german rex
+function factGerman(){
+    alert('A cat will tremble or shiver when it is extreme pain.');
+}
+//Havana
+function factHavana(){
+    alert('All cats have claws, and all except the cheetah sheath them when at rest.');
+}
+//Highlander
+function factHighlander(){
+    alert('Ginger tabby cats can have freckles around their mouths and on their eyelids!');
+}
+//Highland Fold
+function factHighlandFold(){
+    alert('Some notable people who disliked cats:  Napoleon Bonaparte, Dwight D. Eisenhower, Hitler.');
+}
+//Himalayan
+function factHimalayan(){
+    alert('The lightest cat on record is a blue point Himalayan called Tinker Toy, who weighed 1 pound, 6 ounces (616 g). Tinker Toy was 2.75 inches (7 cm) tall and 7.5 inches (19 cm) long.');
+}
+//Kaho Manee
+function factKao(){
+    alert('Cats only use their meows to talk to humans, not each other. The only time they meow to communicate with other felines is when they are kittens to signal to their mother.');
+}
+//kora
+function factKora(){
+    alert('Female cats tend to be right pawed, while male cats are more often left pawed. Interestingly, while 90% of humans are right handed, the remaining 10% of lefties also tend to be male.');
+}
+//la perm
+function factPerm(){
+    alert('Of all the species of cats, the domestic cat is the only species able to hold its tail vertically while walking. All species of wild cats hold their tail horizontally or tucked between their legs while walking.');
+}
+//Main coon
+function factMainCoon(){
+    alert('The Maine Coone is the only native American long haired breed.');
+}
+// Mandarin
+function factMandarin(){
+    alert('There are up to 60 million feral cats in the United States alone.');
+}
+//Manx
+function factManx(){
+    alert('The tiniest cat on record is Mr. Pebbles, a 2-year-old cat that weighed 3 lbs (1.3 k) and was 6.1 inches (15.5 cm) high.');
+}
+//mau arabe
+function factMauArabe(){
+    alert('A cat cannot see directly under its nose.');
+}
+//mau egyptian
+function factMauEgyptian(){
+    alert('In ancient Egypt, mummies were made of cats, and embalmed mice were placed with them in their tombs. In one ancient city, over 300,000 cat mummies were found');
+}
+//minskin
+function factMinskin(){
+    alert('The life expectancy of cats has nearly doubled since 1930 - from 8 to 16 years.');
+}
+//munchkin
+function factMunchkin(){
+    alert('There are approximately 60,000 hairs per square inch on the back of a cat and about 120,000 per square inch on its underside.');
+}
+//nebelung
+function factNebelung(){
+    alert('People who are allergic to cats are actually allergic to cat saliva or to cat dander. If the resident cat is bathed regularly the allergic people tolerate it better.');
+}
+//norvegien
+function factNorvege(){
+    alert('In multi-cat households, cats of the opposite sex usually get along better.');
+}
+//ocicat
+function factOcicat(){
+    alert('Both humans and cats have identical regions in the brain responsible for emotion.');
+}
+//ojo Azules
+function factOjo(){
+    alert('A cat usually has about 12 whiskers on each side of its face.');
+}
+//oriental shorthair
+function factOriental(){
+    alert('The cat who holds the record for the longest non-fatal fall is Andy. He fell from the 16th floor of an apartment building (about 200 ft/.06 km) and survived.');
+}
+//persan
+function factPersan(){
+    alert('Cats walk on their toes.');
+}
+//Peterbald
+function factPeterbald(){
+    alert('"A kitten will typically weigh about 3 ounces at birth.  The typical male housecat will weigh between  7 and 9 pounds, slightly less for female housecats.');
+}
+//pixibob
+function factPixibob(){
+    alert('Tabby cats are thought to get their name from Attab, a district in Baghdad, now the capital of Iraq.');
+}
+//Ragamuffin
+function factRagamuffin(){
+    alert('A cat has the ability to rotate their ears 180 degrees,with the help of 32 muscles that they use to control them.');
+}
+//ragdoll
+function factRagdoll(){
+    alert('The largest cat breed is the Ragdoll. Male Ragdolls weigh between 12 and 20 lbs (5.4-9.0 k). Females weigh between 10 and 15 lbs (4.5-6.8 k).');
+}
+//sacre_de_birmanie
+function factSacre(){
+    alert('A cat\'s nose is as unique as a human\'s fingerprint.');
+}
+//safari
+function factSafari(){
+    alert('Cats often overract to unexpected stimuli because of their extremely sensitive nervous system.');
+}
+//savhanna
+function factSavan(){
+    alert('There is a species of cat smaller than the average housecat. It is native to Africa and it is the Black-footed cat (Felis nigripes). Its top weight is 5.5 pounds.');
+}
+//scottish
+function factScottish(){
+    alert('Tylenol and chocolate are both poisionous to cats.');
+}
+//selkirk
+function factSelkirk(){
+    alert('Spanish-Jewish folklore recounts that Adam’s first wife, Lilith, became a black vampire cat, sucking the blood from sleeping babies. This may be the root of the superstition that a cat will smother a sleeping baby or suck out the child’s breath.');
+}
+//serengeti
+function factSerengeti(){
+    alert('Many cats cannot properly digest cow\'s milk. Milk and milk products give them diarrhea.');
+}
+//Seychellois
+function factSeychel(){
+    alert('A sexually-active feral tom-cat \\owns\\\" an area of about three square miles and \\\"\"sprays\\\"\" to mark his territory with strong smelling urine.');
+}
+//siamois
+function factSiam(){
+    alert('Edward Lear, author of \\The Owl and the Pussycat\\\"\", is said to have had his new house in San Remo built to exactly the same specification as his previous residence, so that his much-loved tabby, Foss, would immediately feel at home.');
+}
+//sibérien
+function factSiberian(){
+    alert('The biggest wildcat today is the Siberian Tiger. It can be more than 12 feet (3.6 m) long (about the size of a small car) and weigh up to 700 pounds (317 kg).');
+}
+//singapura
+function factSinga(){
+    alert('In 1987 cats overtook dogs as the number one pet in America.');
+}
+//skookum
+function factSkoo(){
+    alert('Like birds, cats have a homing ability that uses its biological clock, the angle of the sun, and the Earth\'s magnetic field. A cat taken far from its home can return to it. But if a cat\'s owners move far from its home, the cat can\'t find them.');
+}
+//snow
+function factSnow(){
+    alert('Cats are subject to gum disease and to dental caries. They should have their teeth cleaned by the vet or the cat dentist once a year.');
+}
+//sokoke
+function factSokoke(){
+    alert('In just 7 years, one un-spayed female cat and one un-neutered male cat and their offspring can result in 420,000 kittens.');
+}
+//somali
+function factSomal(){
+    alert('Cat paws act as tempetature regulators, shock absorbers, hunting and grooming tools, sensors, and more');
+}
+//sphynx
+function factSphynx(){
+    alert('Cats are extremely sensitive to vibrations. Cats are said to detect earthquake tremors 10 or 15 minutes before humans can.');
+}
+//thai
+function factThai(){
+    alert('A cat\'s appetite is the barometer of its health. Any cat that does not eat or drink for more than two days should be taken to a vet.');
+}
+//tiffany
+function factTiff(){
+    alert('Normal body temperature for a cat is 102 degrees F');
+}
+//tonkinois
+function factTonk(){
+    alert('Many cats love having their forehead gently stroked.');
+}
+//turc de van
+function factTurc(){
+    alert('Cat bites are more likely to become infected than dog bites.');
+}
+//ural rex
+function factUral(){
+    alert('Cats make about 100 different sounds. Dogs make only about 10.');
+}
+//factYork
+function factYork(){
+    alert('Some common houseplants poisonous to cats include: English Ivy, iris, mistletoe, philodendron, and yew.');
+}
